09ff5ea02aaf47514507a066882a2b380561a616
kidman <thephantomwarrior02@gmail.com>
2023-09-24 22:55:31 +0700

    Make : components; Add : assets;

---
+++ components/SearchManufactures.tsx
@@ -0,0 +1,10 @@
+import { SearchManufacturerProps } from '@/types'
+import React from 'react'
+
+const SearchManufactures = ({manufacturer, setManufacturer} : SearchManufacturerProps ) => {
+  return (
+    <div>SearchManufactures</div>
+  )
+}
+
+export default SearchManufactures
